5a66bfa6bedfd2438bd801e3323cf5ecf129ee42
ana.estvzz@gmail.com <ana.estvzz@gmail.com>
2024-03-06 13:31:58 +0100

    cambios

--- prueba_rama.py
+++ prueba_rama.py
@@ -1,3 +1,13 @@
-lista = [10,15,20]
+lista = [10,20,30]
+print("10")
 for i in lista:
-    print(i)
+    print((i2) - 1) 
+    print("hohhahah")
+
+lista = [14,22,30]
+for i in lista:
+    print(i**2) 
+print("hola")
+lista = [16,20,30]
+for i in lista:
+    print(i2) 
